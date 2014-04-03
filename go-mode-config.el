21bcfdd1d56cfaff79884fedfdbc35ca0469a01e
Jeff Hodges <jeff@somethingsimilar.com>
2014-04-02 21:24:49 -0700

    use goimports to gofmt

--- go-mode-config.el
+++ go-mode-config.el
@@ -1,3 +1,4 @@
+(setq gofmt-command "goimports")
 (add-hook 'before-save-hook 'gofmt-before-save)
 (add-hook 'go-mode-hook (lambda ()
                           (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)))
