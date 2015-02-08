e83cfef49f6edf797ee00a030f7baa31dbef76c6
Jeff Hodges <jeff@somethingsimilar.com>
2015-02-08 03:01:13 -0800

    set M-. to godef, not etags, in go code

--- init/go-mode-config.el
+++ init/go-mode-config.el
@@ -2,5 +2,7 @@
 (add-hook 'before-save-hook 'gofmt-before-save)
 (add-hook 'go-mode-hook (lambda ()
                           (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)))
+(add-hook 'go-mode-hook (lambda ()
+                          (local-set-key (kbd "M-.") #'godef-jump)))
 
 (provide 'go-mode-config)
