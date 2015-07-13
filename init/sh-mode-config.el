e273d8a396ffaf8a75cfb38257d6cc80a2958a1d
Jeff Hodges <jeff@somethingsimilar.com>
2015-07-13 12:24:12 -0700

    okay sh-mode indent for real this time

--- init/sh-mode-config.el
+++ init/sh-mode-config.el
@@ -1,4 +1,5 @@
-(add-hook 'sh-mode-hook (lambda () (setq indent-tabs-mode f)))
+(add-hook 'sh-mode-hook (lambda () (setq indent-tabs-mode nil)))
 (add-hook 'sh-mode-hook (lambda () (setq sh-basic-offset 2)))
+(add-hook 'sh-mode-hook (lambda () (setq sh-indentation 2)))
 
 (provide 'sh-mode-config)
