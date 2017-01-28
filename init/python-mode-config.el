320f0c29d1cfb33dd85a1d9f92237e129817fd48
Jeff Hodges <jeff@somethingsimilar.com>
2017-01-27 16:13:49 -0800

    fix up bazel python-modes

--- init/python-mode-config.el
+++ init/python-mode-config.el
@@ -1,3 +1,6 @@
-(add-to-list 'auto-mode-alist '("\\BUILD\\'" . python-mode))
+(add-to-list 'auto-mode-alist '("BUILD\\'" . python-mode))
+(add-to-list 'auto-mode-alist '("WORKSPACE\\'" . python-mode))
+(add-to-list 'auto-mode-alist '("\\.bzl\\'" . python-mode))
+(add-to-list 'auto-mode-alist '("\\.bazel\\'" . python-mode))
 
 (provide 'python-mode-config)
