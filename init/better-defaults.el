637f77d88d6b8cdace03d4428321802489cd91c7
Jeff Hodges <jeff@somethingsimilar.com>
2019-09-17 12:08:58 -0700

    upgrade better-defaults.el

--- init/better-defaults.el
+++ init/better-defaults.el
@@ -1,10 +1,10 @@
 ;;; better-defaults.el --- Fixing weird quirks and poor defaults
 
-;; Copyright © 2013 Phil Hagelberg
+;; Copyright © 2013-2019 Phil Hagelberg and contributors
 
 ;; Author: Phil Hagelberg
 ;; URL: https://github.com/technomancy/better-defaults
-;; Version: 0.1.2
+;; Version: 0.1.4
 ;; Created: 2013-04-16
 ;; Keywords: convenience
 
@@ -41,16 +41,22 @@
 
 ;;; Code:
 
-;;;###autoload
 (progn
-  (ido-mode t)
-  (setq ido-enable-flex-matching t)
+  (unless (or (fboundp 'helm-mode) (fboundp 'ivy-mode))
+    (ido-mode t)
+    (setq ido-enable-flex-matching t))
 
-  (menu-bar-mode -1)
+  (unless (eq window-system 'ns)
+    (menu-bar-mode -1))
   (when (fboundp 'tool-bar-mode)
     (tool-bar-mode -1))
   (when (fboundp 'scroll-bar-mode)
     (scroll-bar-mode -1))
+  (when (fboundp 'horizontal-scroll-bar-mode)
+    (horizontal-scroll-bar-mode -1))
+
+  (autoload 'zap-up-to-char "misc"
+    "Kill up to, but not including ARGth occurrence of CHAR." t)
 
   (require 'uniquify)
   (setq uniquify-buffer-name-style 'forward)
@@ -60,6 +66,7 @@
 
   (global-set-key (kbd "M-/") 'hippie-expand)
   (global-set-key (kbd "C-x C-b") 'ibuffer)
+  (global-set-key (kbd "M-z") 'zap-up-to-char)
 
   (global-set-key (kbd "C-s") 'isearch-forward-regexp)
   (global-set-key (kbd "C-r") 'isearch-backward-regexp)
@@ -68,11 +75,13 @@
 
   (show-paren-mode 1)
   (setq-default indent-tabs-mode nil)
-  (setq select-enable-clipboard t
-        select-enable-primary t
-        save-interprogram-paste-before-kill t
+  (setq save-interprogram-paste-before-kill t
         apropos-do-all t
         mouse-yank-at-point t
+        require-final-newline t
+        visible-bell t
+        load-prefer-newer t
+        ediff-window-setup-function 'ediff-setup-windows-plain
         save-place-file (concat user-emacs-directory "places")
         backup-directory-alist `(("." . ,(concat user-emacs-directory
                                                  "backups")))))
