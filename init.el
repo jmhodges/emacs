944dd94f3e9583f06e052b1c212e91b6933131ea
Jeff Hodges <jeff@somethingsimilar.com>
2019-10-27 16:49:16 -0700

    use MELPA and set up use-package auto-install

--- init.el
+++ init.el
@@ -1,23 +1,20 @@
 (add-to-list 'load-path "~/.emacs.d/init")
 
 (require 'package)
-(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
-                    (not (gnutls-available-p))))
-       (proto (if no-ssl "http" "https")))
-  (when no-ssl
-    (warn "\
-Your version of Emacs does not support SSL connections,
-which is unsafe because it allows man-in-the-middle attacks.
-There are two things you can do about this warning:
-1. Install an Emacs version that does support SSL and be safe.
-2. Remove this warning from your init file so you won't see it again."))
-  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
-  ;; (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
-  (add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
-  (when (< emacs-major-version 24)
-    ;; For important compatibility libraries like cl-lib
-    (add-to-list 'package-archives (cons "gnu" (concat proto "://elpa.gnu.org/packages/")))))
+(setq package-enable-at-startup nil)
+(setq package-archives
+      '(("MELPA Stable" . "https://stable.melpa.org/packages/")
+      ("MELPA"        . "https://melpa.org/packages/"))
+      package-archive-priorities
+      '(("MELPA Stable" . 0)
+        ("MELPA"        . 10))) ;; MELPA goes first, then look up Stable
 (package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(eval-when-compile
+  (require 'use-package))
 
 ;; Default width of tab characters to 4 spaces
 (custom-set-variables
